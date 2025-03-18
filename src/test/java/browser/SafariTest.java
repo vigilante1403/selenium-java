fbd388d53ab38449bbfff874996f47f80519844c
vigilante1403 <116486651+vigilante1403@users.noreply.github.com>
2025-03-18 12:48:15 +0700

    push code

---
+++ src/test/java/browser/SafariTest.java
@@ -0,0 +1,12 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SafariTest {
+
+}

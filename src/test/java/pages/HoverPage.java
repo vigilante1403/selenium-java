14f1f286accdf3cfe8fb1675bf0d5f3916acc7b5
vigilante1403 <116486651+vigilante1403@users.noreply.github.com>
2025-05-17 15:58:00 +0700

    update hoverTest and Page

--- src/test/java/pages/HoverPage.java
+++ src/test/java/pages/HoverPage.java
@@ -1,16 +1,22 @@
 package pages;
 
 import org.openqa.selenium.By;
+import utils.Browser;
 
 public class HoverPage {
     public void open(){
         utils.Browser.visit("https://the-internet.herokuapp.com/hovers");
     }
 
-    public void hoverOverElement(String element){
+    public void hoverOverElement(){
         utils.Browser.action.moveToElement(utils.Browser.getElement(By.xpath("//div[@class='figure'][1]"))).perform();
     }
-
+    public void viewProfile(){
+        utils.Browser.action.click(Browser.getElement(By.xpath("(//div[@class='figure'][1])/div[@class='figcaption']/a[.='View profile']"))).perform();
+    }
+    public void confirmElement(){
+        Browser.visible(By.tagName("h1"));
+    }
     public String getTextOfElement(String element){
         return utils.Browser.getElement(By.xpath("//div[@class='figure'][1]//h5")).getText();
     }
